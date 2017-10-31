a6fdab26cca203fd009665ee1b624b6498c07cbf
ivdekov <ivdekov@gmail.com>
2017-10-31 16:29:02 -0500

    Fix indendation; add comment for schedule post type code

--- colby-wp-schedule.php
+++ colby-wp-schedule.php
@@ -11,27 +11,28 @@
  * @package colby-wp-schedule
  */
 
- add_action( 'init', function() {
-   $labels = array(
- 		'name'               => _x( 'Schedules', 'post type general name', 'colby-wp-schedule' ),
- 		'singular_name'      => _x( 'Schedule', 'post type singular name', 'colby-wp-schedule' ),
- 		'add_new_item'       => __( 'Add New Schedule', 'colby-wp-schedule' ),
- 		'new_item'           => __( 'New Schedule', 'colby-wp-schedule' ),
- 		'edit_item'          => __( 'Edit Schedule', 'colby-wp-schedule' ),
- 		'view_item'          => __( 'View Schedules', 'colby-wp-schedule' ),
- 		'all_items'          => __( 'All Schedules', 'colby-wp-schedule' ),
- 		'search_items'       => __( 'Search Schedules', 'colby-wp-schedule' ),
- 		'not_found'          => __( 'No web docs found.', 'colby-wp-schedule' ),
- 	);
+// Add schedule custom post type
+add_action( 'init', function() {
+ $labels = array(
+		'name'               => _x( 'Schedules', 'post type general name', 'colby-wp-schedule' ),
+		'singular_name'      => _x( 'Schedule', 'post type singular name', 'colby-wp-schedule' ),
+		'add_new_item'       => __( 'Add New Schedule', 'colby-wp-schedule' ),
+		'new_item'           => __( 'New Schedule', 'colby-wp-schedule' ),
+		'edit_item'          => __( 'Edit Schedule', 'colby-wp-schedule' ),
+		'view_item'          => __( 'View Schedules', 'colby-wp-schedule' ),
+		'all_items'          => __( 'All Schedules', 'colby-wp-schedule' ),
+		'search_items'       => __( 'Search Schedules', 'colby-wp-schedule' ),
+		'not_found'          => __( 'No web docs found.', 'colby-wp-schedule' ),
+	);
 
- 	$args = array(
- 		'labels'             => $labels,
- 		'public'             => true,
- 		'supports'           => array( 'title' )
- 	);
+	$args = array(
+		'labels'             => $labels,
+		'public'             => true,
+		'supports'           => array( 'title' )
+	);
 
- 	register_post_type( 'schedule', $args );
- } );
+	register_post_type( 'schedule', $args );
+} );
 
 // Display IDs for schedule custom posts
 add_filter( 'manage_schedule_posts_columns', 'revealid_add_id_column', 5 );
