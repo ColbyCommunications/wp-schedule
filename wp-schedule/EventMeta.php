8989c06c89b6de7bbc908729143eb678c04a698f
johnwatkins0 <johnwatkins0@gmail.com>
2017-12-13 19:44:49 -0500

    Use Carbon instead of Titan

--- wp-schedule/EventMeta.php
+++ wp-schedule/EventMeta.php
@@ -5,41 +5,35 @@
 
 namespace Colby\Schedules;
 
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
 /**
  * Add hooks to register meta fields for events posts.
  */
 class EventMeta {
 	public function __construct() {
+		\Carbon_Fields\Carbon_Fields::boot();
 
-		$this->titan = \TitanFramework::getInstance( 'colby-wp-schedule' );
-
-		add_action( 'tf_create_options', [ $this, 'register_details_meta_box' ] );
-		add_action( 'tf_create_options', [ $this, 'register_details_location_field' ] );
+		add_action( 'carbon_fields_register_fields', [ $this, 'register_details_meta_box' ] );
+		add_action( 'carbon_fields_register_fields', [ $this, 'register_fields' ] );
 	}
 
 	/**
 	 * Creates the box that will contain meta fields.
 	 */
 	public function register_details_meta_box() {
-		$this->details_box = $this->titan->createMetaBox(
-			[
-				'name' => 'Event Details',
-				'post_type' => 'schedule', // this will change when new post type is registered
-
-			]
-		);
+		$this->details_box = Container::make( 'post_meta', 'Event Details' )
+			->where( 'post_type', '=', 'schedule' ); // this will change when new post type is registered
 	}
 
 	/**
 	 * Adds the location field to the details box.
 	 */
-	public function register_details_location_field() {
-		$this->details_box->createOption(
+	public function register_fields() {
+		$this->details_box->add_fields(
 			[
-				'name' => 'Location',
-				'id' => 'event-location',
-				'type' => 'text',
-				'desc' => 'Enter the event location',
+				Field::make( 'text', 'colby_schedule__location', 'Location' ),
 			]
 		);
 	}
