47d2e166ae8e30739a26bea0efdd480e8410f2ea
ivdekov <ivdekov@gmail.com>
2017-12-19 15:21:48 -0600

    Add missing space

--- wp-schedule/EventMeta.php
+++ wp-schedule/EventMeta.php
@@ -13,7 +13,7 @@ use Carbon_Fields\Field;
  */
 class EventMeta {
 	public function __construct() {
-		add_action( 'after_setup_theme', [ $this, 'boot_carbon_fields'] );
+		add_action( 'after_setup_theme', [ $this, 'boot_carbon_fields' ] );
 		add_action( 'carbon_fields_register_fields', [ $this, 'register_details_meta_box' ] );
 		add_action( 'carbon_fields_register_fields', [ $this, 'register_fields' ] );
 	}
