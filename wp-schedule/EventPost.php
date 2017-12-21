9ebf293af96e6d314f1c1f1a5db11dd2cce5be55
ivdekov <ivdekov@gmail.com>
2017-12-21 13:22:19 -0600

    Add base CustomPostType class; have Schedule and Event classes inherit it

--- wp-schedule/EventPost.php
+++ wp-schedule/EventPost.php
@@ -1,66 +1,62 @@
 <?php
 /**
  * EventPost.php
+ *
+ * @package colby-wp-schedule
  */
 
 namespace Colby\Schedules;
 
-class EventPost {
-  private $post_type = 'event';
-  private $post_type_ucfirst;
+/**
+ * Setup 'event' custom post type.
+ */
+class EventPost extends CustomPostType {
+	/**
+	 * Constructor function.
+	 */
+	public function __construct() {
+		parent::__construct( 'event' );
 
-  public function __construct() {
-    $this->post_type_ucfirst = ucfirst( $this->post_type );
-    add_action( 'init', [ $this, 'register_event_post_type'] );
-    add_action( 'init', [ $this, 'register_custom_taxonomies' ] );
-  }
+		add_action( 'init', [ $this, 'register_event_post_type' ] );
+		add_action( 'init', [ $this, 'register_custom_taxonomies' ] );
+	}
 
-  /**
-   * Create 'event' custom post type
-   */
-  public function register_event_post_type() {
-    $labels = [
-			'name'               => _x( "{$this->post_type_ucfirst}s", 'post type general name', 'colby-wp-schedule' ),
-			'singular_name'      => _x( "{$this->post_type_ucfirst}", 'post type singular name', 'colby-wp-schedule' ),
-			'add_new_item'       => __( "Add New {$this->post_type_ucfirst}", 'colby-wp-schedule' ),
-			'new_item'           => __( "New {$this->post_type_ucfirst}", 'colby-wp-schedule' ),
-			'edit_item'          => __( "Edit {$this->post_type_ucfirst}", 'colby-wp-schedule' ),
-			'view_item'          => __( "View {$this->post_type_ucfirst}s", 'colby-wp-schedule' ),
-			'all_items'          => __( "All {$this->post_type_ucfirst}", 'colby-wp-schedule' ),
-			'search_items'       => __( "Search {$this->post_type_ucfirst}s", 'colby-wp-schedule' ),
-			'not_found'          => __( "No {$this->post_type_ucfirst}s found.", 'colby-wp-schedule' ),
+	/**
+	 * Create 'event' custom post type.
+	 */
+	public function register_event_post_type() {
+		register_post_type( $this->post_type, $this->post_type_args );
+	}
+
+	/**
+	 * Generate labels array for specified taxonomy.
+	 *
+	 * @param string $taxonomy Name of the taxonomy.
+	 */
+	private function get_taxonomy_label_args( $taxonomy ) {
+		return [
+			'labels' => [
+				'name'          => "{$taxonomy}s",
+				'singular_name' => "{$taxonomy}",
+				'add_new_item'  => "Add New {$taxonomy}",
+				'search_items'  => "Search {$taxonomy}s",
+			],
 		];
+	}
 
-		$args = array(
-			'labels'             => $labels,
-			'show_ui'            => true,
-			'supports'           => array( 'title' ),
+	/**
+	 * Register taxonomies for 'event' post type.
+	 */
+	public function register_custom_taxonomies() {
+		$schedule_taxonomy_args = array_merge(
+			$this->get_taxonomy_label_args( 'Schedule' ), [ 'hierarchical' => true ]
 		);
 
-		register_post_type( $this->post_type, $args );
-  }
+		register_taxonomy( 'schedule', $this->post_type, $schedule_taxonomy_args );
+		register_taxonomy( 'event_tag', $this->post_type, $this->get_taxonomy_label_args( 'Event Tag' ) );
 
-  private function get_taxonomy_label_args( $taxonomy ) {
-    return [
-      'labels' => [
-        'name'          => _x( "{$taxonomy}s", 'taxonomy general name' ),
-        'singular_name' => _x( "{$taxonomy}", 'taxonomy singular name' ),
-        'add_new_item'  => __( "Add New {$taxonomy}" ),
-        'search_items'  => __( "Search {$taxonomy}s" )
-      ]
-    ];
-  }
-
-  public function register_custom_taxonomies() {
-    $schedule_taxonomy_args = array_merge(
-      $this->get_taxonomy_label_args( 'Schedule' ), [ 'hierarchical' => true ]
-    );
-
-    register_taxonomy( 'schedule', $this->post_type, $schedule_taxonomy_args );
-    register_taxonomy( 'event_tag', $this->post_type, $this->get_taxonomy_label_args('Event Tag') );
-
-    register_taxonomy_for_object_type( 'schedule', $this->post_type );
-    register_taxonomy_for_object_type( 'event_tag', $this->post_type );
-  }
+		register_taxonomy_for_object_type( 'schedule', $this->post_type );
+		register_taxonomy_for_object_type( 'event_tag', $this->post_type );
+	}
 
 }
--- wp-schedule/SchedulePost.php
+++ wp-schedule/SchedulePost.php
@@ -1,68 +1,61 @@
 <?php
 /**
  * SchedulePost.php
+ *
+ * @package colby-wp-schedule
  */
 
 namespace Colby\Schedules;
 
-class SchedulePost {
-  public function __construct() {
-    add_action( 'init', [ $this, 'register_schedule_post_type'] );
-    add_filter( 'manage_schedule_posts_columns', [ $this, 'revealid_add_id_column' ], 5 );
-    add_action( 'manage_schedule_posts_custom_column', [ $this, 'revealid_id_column_content' ], 5, 2 );
-    add_shortcode( 'schedule', [ $this, 'handle_schedule_shortcode' ] );
-  }
+/**
+ * Setup 'schedule' custom post type.
+ */
+class SchedulePost extends CustomPostType {
+	/**
+	 * Call parent constructor. Add all hooks.
+	 */
+	public function __construct() {
+		parent::__construct( 'schedule' );
 
-  /**
-   * Create 'schedule' custom post type
-   */
-  public function register_schedule_post_type() {
-    $labels = array(
-			'name'               => _x( 'Schedules', 'post type general name', 'colby-wp-schedule' ),
-			'singular_name'      => _x( 'Schedule', 'post type singular name', 'colby-wp-schedule' ),
-			'add_new_item'       => __( 'Add New Schedule', 'colby-wp-schedule' ),
-			'new_item'           => __( 'New Schedule', 'colby-wp-schedule' ),
-			'edit_item'          => __( 'Edit Schedule', 'colby-wp-schedule' ),
-			'view_item'          => __( 'View Schedules', 'colby-wp-schedule' ),
-			'all_items'          => __( 'All Schedules', 'colby-wp-schedule' ),
-			'search_items'       => __( 'Search Schedules', 'colby-wp-schedule' ),
-			'not_found'          => __( 'No web docs found.', 'colby-wp-schedule' ),
-		);
+		add_action( 'init', [ $this, 'register_schedule_post_type' ] );
+		add_filter( 'manage_schedule_posts_columns', [ $this, 'revealid_add_id_column' ], 5 );
+		add_action( 'manage_schedule_posts_custom_column', [ $this, 'revealid_id_column_content' ], 5, 2 );
+	}
 
-		$args = array(
-			'labels'             => $labels,
-			'show_ui'            => true,
-			'supports'           => array( 'title' ),
-		);
+	/**
+	 * Create 'schedule' custom post type.
+	 */
+	public function register_schedule_post_type() {
+		register_post_type( $this->post_type, $this->post_type_args );
+	}
 
-		register_post_type( 'schedule', $args );
-  }
+	/**
+	 * Add column to schedule posts for displaying IDs.
+	 *
+	 * @param array $columns Array of data columns
+	 * on display when viewing list of all posts.
+	 */
+	public function revealid_add_id_column( $columns ) {
+		// Make IDs display in the second column, after the post's title.
+		$checkbox = array_slice( $columns, 0, 2 );
+		$columns = array_slice( $columns, 1 );
 
-  /**
-   * Add column to schedule posts for displaying IDs
-   */
-  public function revealid_add_id_column( $columns ) {
-    // Make IDs display in the second column, after the post's title
-  	$checkbox = array_slice( $columns , 0, 2 );
-  	$columns = array_slice( $columns , 1 );
+		$id['revealid_id'] = 'ID';
 
-  	$id['revealid_id'] = 'ID';
+		$columns = array_merge( $checkbox, $id, $columns );
+		return $columns;
+	}
 
-  	$columns = array_merge( $checkbox, $id, $columns );
-  	return $columns;
-  }
-
-  /**
-  * Display IDs for 'schedule' custom posts
-  */
-  public function revealid_id_column_content( $column, $id ) {
-  	if ( 'revealid_id' == $column ) {
-  		echo $id;
-  	}
-  }
-
-  public function handle_schedule_shortcode() {
-    // TODO: implement this method
-  }
+	/**
+	 * Display IDs for 'schedule' custom posts.
+	 *
+	 * @param string  $column Name of column.
+	 * @param integer $id Post id.
+	 */
+	public function revealid_id_column_content( $column, $id ) {
+		if ( 'revealid_id' === $column ) {
+			echo $id;
+		}
+	}
 
 }
